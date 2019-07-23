fdba11639365fc1aea01c02f293c284d471417d7
Steven Pawley <dr.stevenpawley@gmail.com>
2019-07-23 08:10:02 +0000

    version 0.0.1

---
+++ R/utils.R
@@ -0,0 +1,143 @@
+#' Calculate the T_SLOPE value based on DEM resolution for MRVBF
+#'
+#' Calculates the T_SLOPE value for the Multiresolution Index of Valley Bottom
+#' Flatness (Gallant and Dowling, 2003) based on input DEM resolution. MRVBF
+#' identified valley bottoms based on classifying slope angle and identifying
+#' low areas by ranking elevation in respect to the surrounding topography
+#' across a range of DEM resolutions. The MRVBF algorithm was developed using a
+#' 25 m DEM, and so if the input DEM has a different resolution then the slope
+#' threshold T_SLOPE needs to be adjusted from its default value of 16 in order
+#' to maintain the relationship between slope and DEM resolution. This function
+#' provides a convenient way to perform that calculation.
+#'
+#' @param res Input DEM resolution
+#' @param plot logical, produce plot of relationship
+#'
+#' @return Numeric. T_SLOPE value for MRVBF
+#' @export
+#'
+#' @examples
+#' mrvbf_threshold(res = 10, plot = TRUE)
+mrvbf_threshold <- function(res, plot = FALSE) {
+  # slope decreases by factor of 2 per every 'step' above a 25 m dem resolution
+  # a step consists of a 3 factor increase in the dem cell size
+  # Gallant and Dowling 2003
+  dem_res <- c(6075, 2025, 675, 225, 75, 25, 8, 3, 1)
+  mrvbf_slope <- c(0.5, 1, 2, 4, 8, 16, 32, 64, 128)
+  ds <- data.frame(dem_res, mrvbf_slope)
+  m <- minpack.lm::nlsLM(mrvbf_slope ~ a * I(dem_res^z), data = ds, start = list(a = 100, z = 1))
+
+  if (plot == TRUE) {
+    # produce nls smooth line
+    predx <- seq(min(res, dem_res / 2), max(res * 2, dem_res), 0.1)
+    predy <- stats::predict(m, list(dem_res = predx))
+
+    # plot
+    graphics::plot(dem_res, mrvbf_slope,
+      xlab = "DEM resolution (m)",
+      ylab = "MRVBF Initial Slope", xlim = c(min(predx), max(min(dem_res), res) * 1.5), xaxs = "i"
+    )
+    graphics::lines(predx, predy)
+    graphics::lines(x = c(res, res), y = c(0, stats::predict(m, list(dem_res = res))))
+    graphics::lines(x = c(0, res), y = c(stats::predict(m, list(dem_res = res)), stats::predict(m, list(dem_res = res))))
+  }
+
+  val <- stats::predict(m, list(dem_res = res))
+  as.numeric(val)
+}
+
+
+#' Search for a SAGA-GIS tool
+#'
+#' @param x saga object
+#' @param pattern character. Pattern of text to search for within the tool name
+#'
+#' @return dataframe of tools that match the pattern of the search text and
+#' their host library
+#' @export
+#' @examples
+#' \dontrun{
+#' # initialize Rsagacmd
+#' saga <- sagaGIS()
+#'
+#' # search for a tool
+#' search_tools(x = saga, pattern = "Terrain")
+#' }
+search_tools <- function(x, pattern) {
+
+  # get local environment of sagaGIS object (first tool)
+  env <- environment(x[[1]][[1]])
+
+  matches <- list()
+
+  for (lib in names(env$tool_libraries)) {
+    match_text <- grep(pattern, names(env$tool_libraries[[lib]]), ignore.case = TRUE)
+    if (length(match_text) > 0) {
+      matches[[lib]] <- names(env$tool_libraries[[lib]])[match_text]
+    }
+  }
+
+  matches_df <- data.frame(
+    library = rep(names(matches), lapply(matches, length)),
+    tool = unlist(matches, use.names = FALSE)
+  )
+  return(matches_df)
+}
+
+
+#' Split a raster grid into tiles for tile-based processing
+#'
+#' Split a raster grid into tiles. The tiles are saved as Rsagacmd
+#' temporary files, and are loaded as a list of R objects for futher
+#' processing. This is a function to make the the SAGA-GIS
+#' grid_tools / tiling tool more convenient to use.
+#'
+#' @param x saga S3 object
+#' @param grid character, or RasterLayer. GDAL-supported raster, as as a path to
+#'   the raster or representing a RasterLayer object
+#' @param nx numeric. Number of x-pixels per tile
+#' @param ny numeric. Number of y-pixels per tile
+#' @param overlap numeric. Number of overlapping pixels
+#' @param file_path character. Optional file path to store raster tiles
+#'
+#' @return list. List of RasterLayer objects representing tiled data
+#' @export
+#' @examples
+#' \dontrun{
+#' # Initialize a saga object
+#' saga <- sagaGIS()
+#'
+#' # Generate a random DEM
+#' dem <- saga$grid_calculus$Random_Terrain(RADIUS = 15, ITERATIONS = 500)
+#'
+#' # Return tiled version of DEM
+#' tiles <- tileGeoprocessor(x = saga, grid = dem, nx = 20, ny = 20)
+#' }
+tile_geoprocessor <- function(x, grid, nx, ny, overlap = 0, file_path = NULL) {
+
+  # get local environment of sagaGIS object (first tool)
+  env <- environment(x[[1]][[1]])
+
+  # calculate number of tiles required
+  n_widths <- ceiling(1 / (nx / (ncol(grid) + overlap)))
+  n_heights <- ceiling(1 / (ny / (nrow(grid) + overlap)))
+  n_tiles <- n_widths * n_heights
+
+  # create list of temporary files for tiles
+  tile_outputs <- c()
+  for (i in 1:n_tiles) {
+    if (is.null(file_path)) {
+      temp <- tempfile(fileext = ".sgrd")
+    } else {
+      temp <- tempfile(tmpdir = file_path, fileext = ".sgrd")
+    }
+    tile_outputs <- c(tile_outputs, temp)
+    pkg.env$sagaTmpFiles <- append(pkg.env$sagaTmpFiles, temp)
+  }
+
+  # grid tiling
+  saga_execute(
+    lib = "grid_tools", tool = "tiling", senv = env$senv, intern = TRUE, all_outputs = TRUE,
+    list(GRID = grid, TILES = tile_outputs, OVERLAP = overlap, NX = nx, NY = ny)
+  )
+}
---
+++ tests/testthat/test-saga_environment.R
@@ -0,0 +1,73 @@
+context("test-saga_environment")
+library(Rsagacmd)
+library(raster)
+
+testthat::test_that("Initiation of saga S3 class ", {
+  testthat::skip_on_cran()
+
+  if (!is.null(saga_search())) {
+    saga <- saga_gis()
+    testthat::expect_true(!is.null(saga)) # check that saga class was produced
+    testthat::expect_is(saga, "saga") # check that saga is a S3 class saga
+    testthat::expect_gt(length(saga), 0) # check that saga contains libraries
+  }
+})
+
+
+testthat::test_that("Initiation of saga S3 class using opt_lib ", {
+  testthat::skip_on_cran()
+
+  if (!is.null(saga_search())) {
+    saga1 <- saga_gis(opt_lib = "climate_tools")
+    testthat::expect_true(!is.null(saga1))
+    testthat::expect_length(saga1, n = 1)
+  }
+})
+
+
+testthat::test_that("Test file caching ", {
+  testthat::skip_on_cran()
+  saga_bin <- saga_search()
+
+  if (!is.null(saga_bin)) {
+    saga_version <- Rsagacmd:::saga_version(saga_cmd = saga_bin)
+
+    if (saga_version < as.numeric_version("4.0.0")) {
+
+      # check that file caching is only possible for saga versions > 4.0.0
+      output <- "Cannot enable grid caching or change number cores for SAGA-GIS versions < 4.0.0. Please use a more recent version of SAGA-GIS"
+      testthat::expect_message(saga_gis(grid_caching = T, grid_cache_threshold = 20), output)
+    } else {
+
+      # check that saga S3 class can be initiated using file caching
+      saga_fc <- saga_gis(grid_caching = T, grid_cache_threshold = 0.001)
+      testthat::expect_true(!is.null(saga_fc))
+      testthat::expect_is(saga_fc, "saga")
+      testthat::expect_gt(length(saga_fc), 0)
+
+      # check that file caching is working by checking time for running a process
+      # compared to not using file caching
+      saga <- saga_gis()
+      dem <- saga$grid_calculus$random_terrain(
+        TARGET_USER_XMIN = 0,
+        TARGET_USER_XMAX = 1000,
+        TARGET_USER_YMIN = 0,
+        TARGET_USER_YMAX = 1000,
+        RADIUS = 100,
+        ITERATIONS = 500
+      )
+
+      start_time <- Sys.time()
+      tri <- saga$ta_morphometry$terrain_ruggedness_index_tri(DEM = dem)
+      end_time <- Sys.time()
+      elapsed_ram <- end_time - start_time
+
+      start_time <- Sys.time()
+      tri_fc <- saga_fc$ta_morphometry$terrain_ruggedness_index_tri(DEM = dem)
+      end_time <- Sys.time()
+      elapsed_fc <- end_time - start_time
+
+      testthat::expect_true(elapsed_fc > elapsed_ram)
+    }
+  }
+})
